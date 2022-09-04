6084152749682ef5257c58c59c9d82da04ed1e4b
pan <panakeia@protonmail.com>
2022-09-04 22:10:11 +0200

    webserver: Serialize users in camelCase

--- src/user.rs
+++ src/user.rs
@@ -1,6 +1,7 @@
 use serde_derive::{Deserialize, Serialize};
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Debug, PartialEq, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
 pub struct User {
     pub display_name: String,
     pub connect_code: String,
