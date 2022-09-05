fff35d8170fe7069452029d1345625b1d03c8eb3
pan <panakeia@protonmail.com>
2022-09-06 00:03:52 +0200

    Add another fake user in webserver.rs

--- src/webserver.rs
+++ src/webserver.rs
@@ -10,16 +10,24 @@ pub async fn start_server(host: IpAddr, port: u16) {
     let get_user = warp::get()
         .and(warp::path("user"))
         .and(warp::path::param::<String>())
-        .map(|_| {
+        .map(|uid| {
             println!("Received web request");
 
-            let test_user: User = User {
-                display_name: "test".to_string(),
-                connect_code: "TEST#001".to_string(),
-                latest_version: "2.5.1".to_string(),
-            };
-
-            warp::reply::json(&test_user)
+            if uid == "1" {
+                let test_user: User = User {
+                    display_name: "test".to_string(),
+                    connect_code: "TEST#001".to_string(),
+                    latest_version: "2.5.1".to_string(),
+                };
+                warp::reply::json(&test_user)
+            } else {
+                let test_user: User = User {
+                    display_name: "test-2".to_string(),
+                    connect_code: "TEST#002".to_string(),
+                    latest_version: "2.5.1".to_string(),
+                };
+                warp::reply::json(&test_user)
+            }
         });
 
     tokio::spawn(warp::serve(get_user).run(socket_address))
