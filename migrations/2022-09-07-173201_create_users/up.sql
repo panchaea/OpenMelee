bf906bdba60e084029c0783ff29ebfcbd5b29224
pan <panakeia@protonmail.com>
2022-09-11 10:50:42 +0200

    migrations: make connect_code UNIQUE

--- migrations/2022-09-07-173201_create_users/up.sql
+++ migrations/2022-09-07-173201_create_users/up.sql
@@ -2,6 +2,6 @@ CREATE TABLE users (
     uid VARCHAR PRIMARY KEY NOT NULL,
     play_key VARCHAR NOT NULL,
     display_name VARCHAR NOT NULL,
-    connect_code VARCHAR NOT NULL,
+    connect_code VARCHAR UNIQUE NOT NULL,
     latest_version VARCHAR
 )
